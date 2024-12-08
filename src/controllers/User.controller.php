cc8671f7f43910519393bce257321973083dfcaa
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-12-08 00:23:07 -0300

    Controladores para cada tipo de solicitud

---
+++ src/controllers/User.controller.php
@@ -0,0 +1,26 @@
+<?php
+    
+    class UserController{
+        
+        public static function getAllUser(MySQLConnection $conn){
+            $conn_OK = $conn->getConnection();
+            $query = "SELECT * FROM `user`";
+            $prexe = $conn_OK->prepare($query);
+            try {
+                if($prexe->execute()){
+                    $resultados = $prexe->fetchAll(PDO::FETCH_ASSOC);
+                    header('Content-Type: application/json; charset=UTF-8');
+                    header('HTTP/1.1 200 Consulta ejecutada');
+                    echo json_encode($resultados);
+                    
+                }else{
+                    header('HTTP/1.1 500 Internal Server Error');
+                    echo json_encode(["error" => "Error al ejecutar la consulta."]);
+                }
+            } catch (\Throwable $th) {
+                header('HTTP/1.1 500 Internal Server Error');
+                    echo json_encode(["error" => "Error interno del servidor."]);
+            }
+        }
+    }
+?>
