6ba33b0f5ae89d3fc19cfc5b350530a8837e4daf
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2024-12-09 00:47:15 -0300

    Se añadió controlador para hacer consultas con parametros

--- src/controllers/User.controller.php
+++ src/controllers/User.controller.php
@@ -22,5 +22,33 @@
                     echo json_encode(["error" => "Error interno del servidor."]);
             }
         }
+
+        public static function getOneUser(MySQLConnection $conn, $body){
+            //echo $body;
+            $conn_OK = $conn->getConnection();
+            $query = "SELECT * FROM `user` WHERE `nick_name` = '".$body['nick_name']."' AND `password` = '".$body['password']."'";
+            $prexe = $conn_OK->prepare($query);
+
+            try {
+                if($prexe->execute()){
+                    $resultados = $prexe->fetchAll(PDO::FETCH_ASSOC);
+                    header('Content-Type: application/json; charset=UTF-8');
+                    header('HTTP/1.1 200 Consulta ejecutada');
+                    echo json_encode($resultados);
+                    
+                }else{
+                    header('HTTP/1.1 500 Internal Server Error');
+                    echo json_encode(["error" => "Error al ejecutar la consulta."]);
+                }
+            } catch (\Throwable $th) {
+                header('HTTP/1.1 500 Internal Server Error');
+
+                echo json_encode([
+                    "error" => "Error interno del servidor.",
+                    "query" => $query
+                ]);
+            }
+        }
+
     }
 ?>
