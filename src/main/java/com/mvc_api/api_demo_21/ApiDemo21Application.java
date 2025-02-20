4aa15a6227511127ec417bcc9746308ceffda4b9
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2025-02-20 17:32:31 -0300

    API Rest en Java SpringBoot

---
+++ src/main/java/com/mvc_api/api_demo_21/ApiDemo21Application.java
@@ -0,0 +1,13 @@
+package com.mvc_api.api_demo_21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDemo21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemo21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvc_api/api_demo_21/ApiDemo21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc_api.api_demo_21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemo21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
